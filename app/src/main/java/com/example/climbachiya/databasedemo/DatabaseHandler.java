30f9e98ef780589f52c080facb1f5d0029d0a312
C.limbachiya <limbachiya24@gmail.com>
2016-06-03 15:00:15 +0530

    - insert data

---
+++ app/src/main/java/com/example/climbachiya/databasedemo/DatabaseHandler.java
@@ -0,0 +1,147 @@
+package com.example.climbachiya.databasedemo;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by C.limbachiya on 6/3/2016.
+ */
+public class DatabaseHandler extends SQLiteOpenHelper {
+
+    // All Static variables
+    // Database Version
+    private static final int DATABASE_VERSION = 1;
+
+    // Database Name
+    private static final String DATABASE_NAME = "contactsManager";
+
+    // Contacts table name
+    public static final String TABLE_CONTACTS = "contacts";
+
+    private static DatabaseHandler mInstance;
+    private static SQLiteDatabase sqLiteDatabase;
+
+    // Contacts Table Columns names
+    public static final String KEY_ID = "id";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_GENDER = "gender";
+    public static final String KEY_INTEREST = "interest";
+    public static final String KEY_COURSE = "course";
+
+    public static DatabaseHandler getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new DatabaseHandler(context);
+        }
+        return mInstance;
+    }
+
+    public DatabaseHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_CONTACTS_TABLE = "CREATE TABLE " + TABLE_CONTACTS + "("
+                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_NAME + " VARCHAR(50),"
+                + KEY_EMAIL + " VARCHAR(30), "
+                + KEY_GENDER + " VARCHAR(6), "
+                + KEY_INTEREST + " VARCHAR(30), "
+                + KEY_COURSE + " VARCHAR(50) "
+                + ")";
+        db.execSQL(CREATE_CONTACTS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CONTACTS);
+        // Create tables again
+        onCreate(db);
+    }
+
+    //Insert into tables
+    public long insertQuery(ContentValues values, String TABLE) {
+
+        long returnVal = 0;
+
+        try {
+            sqLiteDatabase = getDatabaseMode("write");
+            // Inserting Row
+            returnVal = sqLiteDatabase.insert(TABLE, null, values);
+        } catch (Exception e) {
+            returnVal = 0;
+            e.printStackTrace();
+        }
+        return returnVal;
+    }
+
+    //Get data by specific records using where clause in query
+    public Cursor getDataByCustomQuery(String QUERY, String[] args) {
+
+        Cursor cursor = null;
+        try {
+
+            sqLiteDatabase = getDatabaseMode("read");
+            cursor = sqLiteDatabase.rawQuery(QUERY, args);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return cursor;
+    }
+
+    //Delete all data of table
+    public void truncateTable(String TABLE) {
+        try {
+            sqLiteDatabase = getDatabaseMode("write");
+            sqLiteDatabase.execSQL("DELETE FROM " + TABLE); //delete all rows in a table
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Delete record
+    public int deleteRecord(String table, String whereClause, String[] whereArgs) {
+
+        int returnVal = 0;
+        try {
+            sqLiteDatabase = getDatabaseMode("write");
+            returnVal = sqLiteDatabase.delete(table, whereClause, whereArgs);
+        } catch (Exception e) {
+            e.printStackTrace();
+            returnVal = 0;
+        }
+
+        return returnVal;
+    }
+
+    /**
+     * Returns a writable database instance in order not to open and close many
+     * SQLiteDatabase objects simultaneously
+     *
+     * @return a writable instance to SQLiteDatabase
+     */
+    public SQLiteDatabase getDatabaseMode(String mode) {
+        if ((sqLiteDatabase == null) || (!sqLiteDatabase.isOpen())) {
+            if (mode.equalsIgnoreCase("read"))
+                sqLiteDatabase = this.getReadableDatabase();
+            else
+                sqLiteDatabase = this.getWritableDatabase();
+        }
+
+        return sqLiteDatabase;
+    }
+
+    @Override
+    public void close() {
+        super.close();
+        if (sqLiteDatabase != null) {
+            sqLiteDatabase.close();
+            sqLiteDatabase = null;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/climbachiya/databasedemo/MainActivity.java
@@ -0,0 +1,249 @@
+package com.example.climbachiya.databasedemo;
+
+import android.app.ProgressDialog;
+import android.content.ContentValues;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtName, edtEmail;
+    Spinner mSpinner;
+    RadioGroup rdbGroup;
+    CheckBox chkAndroid, chkIphone;
+    ArrayAdapter<String> dataAdapter;
+    String gender = null;
+    String interest = "";
+    //class members
+    String businessType[] = {"Automobile", "Food", "Computers", "Education",
+            "Personal", "Travel"};
+
+    DatabaseHandler dbHandler = null;
+    ProgressDialog progress = null;
+    String spinnerItem = null;
+    ArrayList<String> arrInterest;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initUIControls();
+
+        initClassObjects();
+
+        registerEvents();
+    }
+
+    private void registerEvents() {
+        rdbGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // find which radio button is selected
+                if (checkedId == R.id.radio_male) {
+                    gender = "male";
+                } else if (checkedId == R.id.radio_female) {
+                    gender = "female";
+                }
+            }
+
+        });
+
+        chkAndroid.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    arrInterest.add("android");
+                }else{
+                    if(arrInterest.contains("android")){
+                        arrInterest.remove("android");
+                    }
+                }
+
+            }
+        });
+        chkIphone.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                    if (isChecked){
+                        arrInterest.add("iphone");
+                    }else{
+                        if(arrInterest.contains("iphone")){
+                            arrInterest.remove("iphone");
+                        }
+                    }
+
+            }
+        });
+
+        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                spinnerItem = mSpinner.getSelectedItem().toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+
+    //Init custom class objects
+    private void initClassObjects() {
+        progress = new ProgressDialog(this);
+        progress.setMessage("Saving...");
+        progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progress.setIndeterminate(true);
+
+        arrInterest = new ArrayList<>();
+
+        dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, businessType);
+
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        mSpinner.setAdapter(dataAdapter);
+    }
+
+    //Init all UI controls here
+    private void initUIControls() {
+        edtName = (EditText) findViewById(R.id.edit_name);
+        edtEmail = (EditText) findViewById(R.id.edit_email);
+        mSpinner = (Spinner) findViewById(R.id.spinner);
+        rdbGroup = (RadioGroup) findViewById(R.id.radio_group);
+        chkAndroid = (CheckBox) findViewById(R.id.check_android);
+        chkIphone = (CheckBox) findViewById(R.id.check_iphone);
+    }
+
+    public void saveData(View view) {
+
+        String name = edtName.getText().toString();
+        String email = edtEmail.getText().toString();
+
+
+        if (null == name || name.isEmpty()) {
+            Toast.makeText(MainActivity.this, "Name should not be empty", Toast.LENGTH_SHORT).show();
+        } else if (null == email || email.isEmpty()) {
+            Toast.makeText(MainActivity.this, "Email should not be empty", Toast.LENGTH_SHORT).show();
+        } else if (!isEmailValid(email)) {
+            Toast.makeText(MainActivity.this, "Enter valid email", Toast.LENGTH_SHORT).show();
+        } else if (null == gender || gender.isEmpty()) {
+            Toast.makeText(MainActivity.this, "Select gender", Toast.LENGTH_SHORT).show();
+        }else if (null == spinnerItem || spinnerItem.isEmpty()) {
+            Toast.makeText(MainActivity.this, "Select course", Toast.LENGTH_SHORT).show();
+        }else if (null == arrInterest || arrInterest.size() <= 0) {
+            Toast.makeText(MainActivity.this, "Select interest", Toast.LENGTH_SHORT).show();
+        } else {
+
+            //call async for save data in background
+            new SaveAsync(name, email, spinnerItem, gender, arrInterest).execute();
+        }
+    }
+
+    /**
+     * Check for valid email
+     *
+     * @param email - email address
+     * @return true/false
+     */
+    public boolean isEmailValid(String email) {
+        final String EMAIL_PATTERN =
+                "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        final Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        final Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    //Save data asychronously in DB
+    class SaveAsync extends AsyncTask<String, String, String> {
+
+        long result = 0;
+
+        String name;
+        String email;
+        String spinnerItem;
+        String gender;
+        ArrayList<String> arrInterest;
+
+        public SaveAsync(String name, String email, String spinnerItem, String gender, ArrayList<String> arrInterest) {
+            this.name = name;
+            this.email = email;
+            this.spinnerItem = spinnerItem;
+            this.gender = gender;
+            this.arrInterest = arrInterest;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progress.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            ContentValues cv = null;
+            try {
+                cv = new ContentValues();
+                dbHandler = DatabaseHandler.getInstance(MainActivity.this);
+
+                String interestString = "";
+
+                for(int i=0; i<arrInterest.size(); i++){
+                    interestString = interestString +","+arrInterest.get(i);
+                }
+
+                interestString = interestString.startsWith(",") ? interestString.substring(1) : interestString;
+                cv.put(DatabaseHandler.KEY_NAME, name);
+                cv.put(DatabaseHandler.KEY_EMAIL, email);
+                cv.put(DatabaseHandler.KEY_GENDER, gender);
+                cv.put(DatabaseHandler.KEY_INTEREST, interestString);
+                cv.put(DatabaseHandler.KEY_COURSE, spinnerItem);
+
+                result = dbHandler.insertQuery(cv, DatabaseHandler.TABLE_CONTACTS);
+            } catch (Exception e) {
+                result = 0;
+                e.printStackTrace();
+            } finally {
+                if (null != dbHandler) {
+                    dbHandler.close();
+                }
+                if (null != cv) {
+                    cv.clear();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            if(null != progress && progress.isShowing())progress.dismiss();
+
+            if (result > 0) {
+                Toast.makeText(MainActivity.this, "Saved...", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Oops!Something wrong while saving", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
